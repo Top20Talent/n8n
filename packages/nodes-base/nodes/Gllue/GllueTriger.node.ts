1a870679eefda62d5b2058a9f35fb2c42c97e96b
Flash Hu <idisblueflash@gmail.com>
2022-01-12 11:34:58 +0800

    add gllue trigger UI

---
+++ packages/nodes-base/nodes/Gllue/GllueTriger.node.ts
@@ -0,0 +1,56 @@
+import {
+	IHookFunctions,
+	IWebhookFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject,
+	INodeType,
+	INodeTypeDescription,
+	IWebhookResponseData,
+} from 'n8n-workflow';
+
+/*
+import {
+	autofriendApiRequest,
+} from './GenericFunctions';
+
+import {
+	snakeCase,
+} from 'change-case';
+*/
+
+
+export class GllueTrigger implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Gllue Trigger',
+		name: 'gllueTrigger',
+		icon: 'file:gllue.svg',
+		group: ['trigger'],
+		version: 1,
+		subtitle: '={{$parameter["event"]}}',
+		description: 'Handle Gllue events via webhooks',
+		defaults: {
+			name: 'Gllue Trigger',
+			color: '#6ad7b9',
+		},
+		inputs: [],
+		outputs: ['main'],
+		credentials: [],
+		webhooks: [
+			{
+				name: 'default',
+				httpMethod: 'POST',
+				responseMode: 'onReceived',
+				path: 'webhook',
+			},
+		],
+		properties: [],
+	};
+
+	async webhook(this: IWebhookFunctions): Promise<IWebhookResponseData> {
+		return {
+			workflowData: [],
+		};
+	}
+}
