75f4d4f8e3535267c5e940acb090493bd50ac080
Flash Hu <idisblueflash@gmail.com>
2022-01-22 11:20:22 +0800

    remove logs

--- packages/nodes-base/nodes/Gllue/GllueConsentLogic.node.ts
+++ packages/nodes-base/nodes/Gllue/GllueConsentLogic.node.ts
@@ -48,43 +48,25 @@ export class GllueConsentLogic implements INodeType {
 		const credentials = await this.getCredentials('gllueApi') as IDataObject;
 		const timestamp = helpers.getCurrentTimeStamp();
 		const token = helpers.generateTokenWithAESKey(timestamp, credentials.apiUsername, credentials.apiAesKey);
-
 		const gllue = new Gllue(credentials.apiHost as string, token, this.helpers.request);
 		const simpleData = await gllue.getDetail(resource, resourceId,
 			'id,jobsubmission__candidate__email,gllueext_send_terms_cv_sent');
 
-		console.log('DEBUG:response data=', JSON.stringify(simpleData));
-
 		const candidateData = Gllue.extractIdAndEmail(simpleData);
 		const source = item.source as string;
 		const consentService = new ConsentService(this.helpers.request);
-
 		const consented = await consentService.getConsented(candidateData.id, source, EMAIL_CHANNEL);
-		console.log('DEBUG: consented row=', consented);
-
 		const sent = await consentService.getSentIn30Days(candidateData.id, source, EMAIL_CHANNEL);
-		console.log('DEBUG: consent sent in 30 days=', sent);
-
 		const service = new SendEmailOnConsentService(consented, sent, resource, candidateData.cvsentField);
-
-
-		const envVar = process.env.NODE_ENV;
-		console.log('DEBUG: env var=', envVar);
 
 		let emailData = {};
 		if (service.canSendEmail()) {
 			const saved = await consentService.create(candidateData.id, source, EMAIL_CHANNEL);
-			console.log('DEBUG: saved consent', JSON.stringify(saved));
 			const emailService = new EmailNotificationService(this.helpers.request);
 			const email = await emailService.saveConsentEmail(candidateData.email);
 			const track_id = email.track_id;
-			console.log('DEBUG: track_id', track_id);
-
 			const updated = await consentService.updateTrackId(saved.id as string, track_id);
-			console.log('DEBUG: updated=', updated);
-
 			const consentConfirmUrl = buildConsentUrl(saved.id as string);
-			console.log('DEBUG: consent confirm url=', consentConfirmUrl);
 
 			emailData = {
 				senderEmail: CONSENT_FROM_EMAIL,
@@ -99,7 +81,6 @@ export class GllueConsentLogic implements INodeType {
 			};
 		}
 		const responseData = service.canSendEmail() ? emailData : [];
-		console.log('DEBUG: response=', responseData);
 		return [this.helpers.returnJsonArray(responseData)];
 
 	}
