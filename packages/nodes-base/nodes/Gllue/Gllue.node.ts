b90b88e443dc0217ab1855bce3409b89dabfcb59
Flash Hu <idisblueflash@gmail.com>
2022-04-08 08:39:42 +0800

    add function resource

--- packages/nodes-base/nodes/Gllue/Gllue.node.ts
+++ packages/nodes-base/nodes/Gllue/Gllue.node.ts
@@ -8,6 +8,7 @@ import {industryFields, industryOperations} from './IndustryDescription';
 import {contractFields, contractOperations} from './ContractDescription';
 import {userFields, userOperations} from './UserDescriptions';
 import {DEFAULT_PAGE, DEFAULT_PAGINATE_BY} from './constants';
+import {functionFields, functionOperations} from './FunctionDescription';
 
 const helpers = require('./helpers');
 
@@ -52,6 +53,10 @@ export class Gllue implements INodeType {
 						value: 'industry',
 					},
 					{
+						name: 'Function',
+						value: 'function',
+					},
+					{
 						name: 'User',
 						value: 'user',
 					},
@@ -70,6 +75,8 @@ export class Gllue implements INodeType {
 			...cityFields,
 			...industryOperations,
 			...industryFields,
+			...functionOperations,
+			...functionFields,
 			...contractOperations,
 			...contractFields,
 			...userOperations,
@@ -89,7 +96,7 @@ export class Gllue implements INodeType {
 
 		const timestamp = helpers.getCurrentTimeStamp();
 		const token = helpers.generateTokenWithAESKey(timestamp, credentials.apiUsername, credentials.apiAesKey);
-		const urlParams = new UrlParams(filters.query, filters.fields, token, paginateBy,page);
+		const urlParams = new UrlParams(filters.query, filters.fields, token, paginateBy, page);
 		const uriGenerated = helpers.gllueUrlBuilder(credentials.apiHost, resource, operation, urlParams);
 
 		if (resource === 'client') {
@@ -97,6 +104,10 @@ export class Gllue implements INodeType {
 				responseData = await getResponseByUri(uriGenerated, this.helpers.request);
 			}
 		} else if (resource === 'city') {
+			if (operation === 'simple_list_with_ids') {
+				responseData = await getResponseByUri(uriGenerated, this.helpers.request);
+			}
+		} else if (resource === 'function') {
 			if (operation === 'simple_list_with_ids') {
 				responseData = await getResponseByUri(uriGenerated, this.helpers.request);
 			}
